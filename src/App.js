8b236572947a12a09e1d4904daf8786f69d8dbd4
Deepesh Rohit <rdeep6597@gmail.com>
2020-09-22 13:36:05 +0530

    Assignment 6

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import NavigationBar from './component/NavigationBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
