f947b9be8a761d746ef2b292bea6f0e7f4438b11
Deepesh Rohit <rdeep6597@gmail.com>
2020-09-22 10:30:16 +0530

    Table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
-function App() {
+const App = () =>
+{
+  const employee = [
+  { name: "Charlie   "  ,job: "Janitor" },
+  { name: "Mac    "  ,job: "  Bouncer" },
+  { name: "Dee   "  , job: "  Aspiring Actoress" },
+  { name: "Dennis "  , job: "  Bartender" }
+]
+  const renderEmployee = (employee,index) =>{
+    return(
+    <tr key = "index">
+      <td>{employee.name}</td>
+      <td>{employee.job}</td>
+    </tr>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <table >
+  <thead>
+    <tr>
+      <th>Name  </th>
+      <th>Job   </th>
+    </tr>
+  </thead>
+  <tbody>
+   
+   {employee.map(renderEmployee)}
+ 
+  </tbody>
+</table>
     </div>
   );
 }
+
 
 export default App;
